871c3df6abafaaba7302bd5756ffa9e0d4584391
Thomas de Groot <mail@thomasdegroot.com>
2021-03-22 23:09:48 +0100

    removed rest assured

--- src/test/java/nl/jdriven/myapplication/ApplicationTest.java
+++ src/test/java/nl/jdriven/myapplication/ApplicationTest.java
@@ -1,17 +1,25 @@
 package nl.jdriven.myapplication;
 
+import nl.jdriven.myapplication.in.rest.MyRestController;
+import nl.jdriven.myapplication.out.storage.model.Answer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static io.restassured.RestAssured.when;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
 class ApplicationTest {
 
+	@Autowired
+	MyRestController myRestController;
+
 	@Test
 	void contextLoads() {
-		when().request("GET", "/api/ping").then().statusCode(200).body("questionId" ,  equalTo("010917"));
+		Answer result = myRestController.pingPong();
+		assertThat(result.getQuestionId()).isEqualTo("010917");
 	}
 
 }
